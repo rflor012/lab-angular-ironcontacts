14e94b822e51080ba36315457e3666210b164561
Rodrigo Flores <rflor12@gmail.com>
2018-07-24 12:35:42 -0400

    Thank you Alina for the help

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -16,9 +16,8 @@ export class ContactListComponent implements OnInit {
     this.contacts = contactList;
   }
 
-  addContact(){
+  addContact(name, email, phone, image){
     console.log("Add contact has been called");
-    // add contact to contacts list
-    // clear inputs
+
   }
 }
